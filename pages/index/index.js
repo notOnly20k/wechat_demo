e746ede2c0d9a137163b300e36224186b8124b3b
cz <1552916842@qq.com>
2018-07-26 18:51:18 +0800

    sss

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,37 @@
 //index.js
 //获取应用实例
 const app = getApp()
+console.log(app.globalData)
+let aaa = "aa"
+if (aaa) {
+  console.log("a")
+} else {
+  console.log("b")
+}
+
+function jumpToMain(page) {
+  let countTime = 4
+  let interval = setInterval(function() {
+    countTime--
+    page.setData({
+      time: countTime
+    })
+    if (countTime < 1) {
+      clearInterval(interval)
+      wx.redirectTo({
+        url: '../main/main',
+      })
+    }
+  }, 1000)
+
+}
 
 Page({
   data: {
     motto: '小智的demo',
     userInfo: {},
     hasUserInfo: false,
+    time: null,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
@@ -15,13 +40,30 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  // jumpToMain: function() {
+  //   let countTime = 10
+  //   let interval = setInterval(function() {
+  //     countTime--
+  //     console.log(countTime)
+  //     this.setData({
+  //       time: countTime
+  //     })
+  //     if (countTime < 1) {
+  //       clearInterval(interval)
+  //       wx.navigateTo({
+  //         url: '../main/main',
+  //       })
+  //     }
+  //   }, 100)
+  // },
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+      jumpToMain(this)
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -29,6 +71,7 @@ Page({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
+        jumpToMain(this)
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
@@ -39,6 +82,7 @@ Page({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
+          jumpToMain(this)
         }
       })
     }
@@ -50,5 +94,6 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+    jumpToMain(this)
   }
 })
